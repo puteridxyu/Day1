72798f79adf2e45ef27efc46a9c05439ec3817fa
puteridxyu <puteridxyu@gmail.com>
2024-02-09 10:55:35 +0800

    090224

---
+++ src/lesson4/exec/Q4/Main.java
@@ -0,0 +1,9 @@
+package lesson4.exec.Q4;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Logger logger = Logger.getInstance();
+        logger.log("Hello, world!");
+    }
+
+}
